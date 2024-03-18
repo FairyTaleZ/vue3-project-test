a447fe0dfaf0d5dcb1c6adc8f8bf03da23ca51fe
零一\JHong <1>
2024-03-18 17:33:59 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings text-left ml-50px text-red-600 font-500">
+      HELLOWORLD
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// main.ts
+import 'virtual:uno.css'
+
+createApp(App).mount('#app')
